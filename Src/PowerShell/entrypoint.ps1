49a91a1598d4316d002260990bb54fedac053ad6
Rob Bos <raj.bos@gmail.com>
2021-08-14 12:29:31 +0200

    More files searching

--- Src/PowerShell/entrypoint.ps1
+++ Src/PowerShell/entrypoint.ps1
@@ -1,6 +1,10 @@
 Write-Host $PSVersion
 
 Write-Host "Hello World from scriptroot: $PSScriptRoot" 
+ForEach ($file in Get-ChildItem) {
+    Write-Host "- $($file.Name)"
+}
+
 
 Set-Location .\src\
 Write-Host "Files in \src\:"
